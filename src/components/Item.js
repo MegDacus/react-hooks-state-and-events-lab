76daed7c663d7a909da76e4a1191afa8ba05c928
Megan Dacus <meganjustice0609@gmail.com>
2023-04-17 12:27:06 +1000

    Assignment Completed

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 
 function Item({ name, category }) {
+  const [isInCart, setIsInCart] = useState(false)
+  const liClass = isInCart ? "in-cart" : null;
+  const buttonText = isInCart ? "Remove From Cart" : "Add to Cart"
+
+  function handleClick() {
+    setIsInCart(!isInCart)
+  }
+
   return (
-    <li className="">
+    <li className={liClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button onClick={handleClick} className="add">{buttonText}</button>
     </li>
   );
 }
--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React, {useState} from "react";
 import Item from "./Item";
 
 function ShoppingList({ items }) {
+  const [selectedCategory, setSelectedCategory] = useState('All');
+
+ function handleChange (e) {
+    setSelectedCategory(e.target.value);
+  }
+  
+function displayItems() {
+  return items.map((item) => ( item.category === selectedCategory || selectedCategory === "All" ? 
+    <Item key={item.id} name={item.name} category={item.category} /> : null
+    ))
+
+  }
+
   return (
     <div className="ShoppingList">
       <div className="Filter">
-        <select name="filter">
+        <select onChange={handleChange} name="filter">
           <option value="All">Filter by category</option>
           <option value="Produce">Produce</option>
           <option value="Dairy">Dairy</option>
@@ -13,9 +26,7 @@ function ShoppingList({ items }) {
         </select>
       </div>
       <ul className="Items">
-        {items.map((item) => (
-          <Item key={item.id} name={item.name} category={item.category} />
-        ))}
+       {displayItems()}
       </ul>
     </div>
   );
